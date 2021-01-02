e7bebc561376e9c9a56e64b9fa99118a5a0dd6e5
Paul Everitt <pauleveritt@me.com>
2021-01-02 11:01:33 -0500

    Switch to enums.

--- src/wired_injector/injectables.py
+++ src/wired_injector/injectables.py
@@ -6,7 +6,8 @@ then report on them for uses such as generation of Sphinx config directives.
 
 """
 from dataclasses import dataclass, field
-from typing import Callable, Set, Optional, Any
+from enum import Enum
+from typing import Callable, Optional, Any, List
 
 from wired_injector import InjectorRegistry
 
@@ -21,32 +22,45 @@ class Injectable:
     target: Optional[Callable]
     context: Optional[Any]
     use_props: bool
-    area: Optional[str] = None
-    phase: Optional[str] = None
+    area: Optional[Enum] = None
+    phase: Optional[Enum] = None
 
 
 @dataclass
 class Injectables:
     registry: InjectorRegistry
-    items: Set[Injectable] = field(default_factory=set)
+    items: List[Injectable] = field(default_factory=list)
 
     def add(self, injectable: Injectable):
-        self.items.add(injectable)
+        self.items.append(injectable)
 
     def find(
             self,
-            area: Optional[str] = None,
+            area: Optional[Enum] = None,
             by_phase: Optional[bool] = False,
-    ) -> Optional[Set[Injectable]]:
+    ) -> Optional[List[Injectable]]:
         if area is None:
             return self.items
 
-        results = {
+        results = [
             injectable
             for injectable in self.items
             if injectable.area == area
-        }
+        ]
 
         if by_phase:
-            results = set(sorted(results, key=lambda v: v.phase))
+            results = sorted(results, key=lambda v: v.phase.value)
         return results
+
+    def apply_injectable(self, injectable: Injectable):
+        self.registry.register_injectable(
+            for_=injectable.for_,
+            target=injectable.target,
+            context=injectable.context,
+            use_props=injectable.use_props,
+        )
+
+    def apply_injectables(self, areas: Enum, phases: Enum):
+        """ Apply the injectables in groups """
+
+        pass
--- tests/test_injectables.py
+++ tests/test_injectables.py
@@ -1,59 +1,82 @@
+from dataclasses import dataclass
+from enum import Enum
+
 import pytest
 from wired_injector import InjectorRegistry
 from wired_injector.injectables import Injectables, Injectable
 
 
+class Area(Enum):
+    system = 1
+    app = 2
+    plugins = 3
+    site = 4
+
+
+class Phase(Enum):
+    init = 1
+    postinit = 2
+
+
+@dataclass
 class DummyTarget:
-    pass
+    title: str = 'Dummy Target'
 
 
 def test_construction(empty_injectables):
-    assert empty_injectables.items == set()
+    assert empty_injectables.items == []
 
 
 def test_add_injectable(full_injectables):
-    first = full_injectables.items.pop()
+    first = full_injectables.items[0]
     assert DummyTarget == first.for_
 
 
 def test_find_all(full_injectables):
     results = full_injectables.find()
-    assert list(results) == list(full_injectables.items)
+    assert results == full_injectables.items
 
 
 def test_find_area(full_injectables):
-    results = full_injectables.find(area='system')
-    first = list(results)[0]
-    assert 'postinit' == first.phase
+    results = full_injectables.find(area=Area.system)
+    first = results[0]
+    assert Phase.postinit == first.phase
 
 
 def test_find_area_phase(full_injectables):
-    results = full_injectables.find(area='system', by_phase=True)
-    first = list(results)[0]
-    assert 'init' == first.phase
+    results = full_injectables.find(area=Area.system, by_phase=True)
+    first = results[0]
+    assert Phase.init == first.phase
+
+
+def test_apply_injectables(empty_injectables, system_init_one):
+    empty_injectables.apply_injectable(system_init_one)
+    container = empty_injectables.registry.create_injectable_container()
+    result: DummyTarget = container.get(DummyTarget)
+    assert 'Dummy Target' == result.title
 
 
 @pytest.fixture
 def system_init_one():
     return Injectable(
-        for_=DummyTarget, target=DummyTarget, context=DummyTarget,
-        use_props=False, area='system', phase='init',
+        for_=DummyTarget, target=DummyTarget, context=None,
+        use_props=False, area=Area.system, phase=Phase.init,
     )
 
 
 @pytest.fixture
-def system_postinit():
+def system_postinit_one():
     return Injectable(
-        for_=DummyTarget, target=DummyTarget, context=DummyTarget,
-        use_props=False, area='system', phase='postinit',
+        for_=DummyTarget, target=DummyTarget, context=None,
+        use_props=False, area=Area.system, phase=Phase.postinit,
     )
 
 
 @pytest.fixture
 def app_init_one():
     return Injectable(
-        for_=DummyTarget, target=DummyTarget, context=DummyTarget,
-        use_props=False, area='app', phase='init',
+        for_=DummyTarget, target=DummyTarget, context=None,
+        use_props=False, area=Area.app, phase=Phase.init,
     )
 
 
@@ -66,12 +89,12 @@ def empty_injectables() -> Injectables:
 
 @pytest.fixture
 def full_injectables(
-        system_init_one, system_postinit,
+        system_init_one, system_postinit_one,
         app_init_one,
 ) -> Injectables:
     ir = InjectorRegistry()
     i = Injectables(ir)
-    i.add(system_postinit)
+    i.add(system_postinit_one)
     i.add(system_init_one)
     i.add(app_init_one)
     return i
