f1ac8d09889245453f734a7de9bf0e947c12a1bf
Paul Everitt <pauleveritt@me.com>
2021-01-17 16:53:40 -0500

    Start of a field pipeline.

---
+++ src/wired_injector/pipeline2/field_pipeline.py
@@ -0,0 +1,26 @@
+"""
+Process all operators and result in a value, or handle the problem.
+
+"""
+
+from typing import Iterator
+
+from . import Operator, OperatorResult, Pipeline
+
+
+def process_field_pipeline(
+    operators: Iterator[Operator],
+    pipeline: Pipeline,
+) -> OperatorResult:
+    """ Process each operator in the pipeline and return the result """
+
+    # Get the first one
+    result = next(operators)(None, pipeline)
+    while operators:
+        try:
+            operator = next(operators)
+            result = operator(result, pipeline)
+        except StopIteration:
+            return result
+
+    return result
---
+++ tests/pipelines2/test_field_pipeline.py
@@ -0,0 +1,27 @@
+from wired_injector.pipeline2 import Pipeline, OperatorResult
+from wired_injector.pipeline2.field_pipeline import process_field_pipeline
+from wired_injector.pipeline2.operators import Get
+from wired_injector.pipeline2.operator_results import Found, NotFound
+
+from .conftest import DummyLookupClass, DummyContainer
+
+
+def test_field_pipeline_one(
+    dummy_container: DummyContainer,
+    dummy_pipeline: Pipeline,
+) -> None:
+    # Set the lookup value
+    dummy_container.fake_lookups[DummyLookupClass] = DummyLookupClass()
+
+    operators = iter([Get(DummyLookupClass), ])
+    result: OperatorResult = process_field_pipeline(operators=operators, pipeline=dummy_pipeline)
+    assert isinstance(result, Found)
+    assert isinstance(result.value, DummyLookupClass)
+
+
+def test_field_pipeline_not_found(dummy_pipeline: Pipeline) -> None:
+    operators = iter([Get(DummyLookupClass), ])
+    result: OperatorResult = process_field_pipeline(operators=operators, pipeline=dummy_pipeline)
+    assert isinstance(result, NotFound)
+    assert DummyLookupClass == result.value
+
